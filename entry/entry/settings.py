34ec803701a8c6a45822dc5ed8ebf7198f83a027
Omar Ahmed El-Hanafy <106983382+OmarAhmedEl-hanafy@users.noreply.github.com>
2024-05-05 15:13:33 +0300

    Update Settings.py
    add 'rest_framework' at INSTALLED_APPS

--- entry/entry/settings.py
+++ entry/entry/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
+    # 'rest_framework_simplejwt',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
